4161229ca4cfe616f9eb2cea5345923c0581077c
ksj1368 <83360918+ksj1368@users.noreply.github.com>
2022-10-16 14:53:13 +0900

    Update Baek_1459.py

--- Baek_1459.py
+++ Baek_1459.py
@@ -14,20 +14,13 @@ x, y, w, s = map(int, input().split()) # x+y만큼 이동
 
 if w*2 <= s:
     answer = w*(x+y)
-    print(1)
 else:
     answer = min(x,y)*s
-    print(2)
     if w > s:
         if abs(x-y)%2 == 0:
             answer += abs(x-y)*s
-            print(3)
         else:
             answer += (abs(x-y)-1)*s + w
-            print(4)
     else:
         answer += abs(x-y)*w
-        print(5)
-        
-    print(answer)
-
+print(answer)
