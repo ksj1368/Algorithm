66ad0d92a7d6947b2d23557b2ad4c64d64b2260a
SeongJun Jang <83360918+ksj1368@users.noreply.github.com>
2023-09-01 11:31:58 +0900

    [unrated] Title: 덧칠하기, Time: 13.91 ms, Memory: 12.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,20 @@
+def solution(n, m, section):
+    answer = 0
+    section_list = [0] * n
+    
+    for i in section:
+        section_list[i-1] = 1
+        
+    for i in range(n):
+        if section_list[i] == 1:
+            min_idx = i
+            if i + m > n-1:
+                max_idx = n-1
+            else:
+                max_idx = i + m - 1
+                
+            for j in range(min_idx, max_idx+1):
+                if section_list[j] == 1:
+                    section_list[j] = 0
+            answer += 1
+    return answer
