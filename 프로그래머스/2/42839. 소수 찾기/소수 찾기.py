522cb6b25a42d8b5d1cde31c910b33f498f7bb01
SeongJun Jang <83360918+ksj1368@users.noreply.github.com>
2023-10-10 15:25:30 +0900

    [level 2] Title: 소수 찾기, Time: 0.55 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,27 @@
+# 조합 구하는 함수
+# 소수 구하는 함수 
+from itertools import permutations
+import math
+
+def isDecimal(num):
+    if(num <= 1):
+        return False 
+    #에라토스테네스의 체
+    for i in range(2, num//2+1):
+        if num%i == 0:
+            return False    
+    return True
+
+def solution(numbers):
+    numbers = list(numbers)
+    answer = 0
+    dec = []
+    for i in range(1, len(numbers)+1):
+        per = permutations(numbers, i)
+        for p in per:
+            num = int("".join(p))
+            if (num not in dec) and (num != 0):
+                dec.append(num)
+    for num in dec:
+        answer += isDecimal(num)
+    return answer
