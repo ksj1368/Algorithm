58c1044ebb094d26467ebeaf2aa1fb46d9a4c4c2
SeongJun Jang <83360918+ksj1368@users.noreply.github.com>
2023-10-14 23:09:46 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4714.47 ms, Memory: 43.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,9 @@
+def solution(elements):
+    
+    answer = set()
+    elements_len = len(elements)
+    elements = elements*2
+    for i in range(elements_len):
+        for j in range(elements_len):
+            answer.add(sum(elements[i:i+j+1]))
+    return len(answer)
