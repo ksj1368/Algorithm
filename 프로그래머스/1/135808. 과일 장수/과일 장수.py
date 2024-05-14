d5432c5a6f3d3ea2a0c26987487c5bb4509573f2
Seongjun Jang <83360918+ksj1368@users.noreply.github.com>
2024-05-14 11:52:56 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,9 +1,2 @@
 def solution(k, m, score):
-    answer = 0
-    while len(score)%m != 0:
-        s_min = min(score)
-        score.remove(s_min)
-    score = sorted(score)
-    for i in range(0, len(score), m):
-        answer += score[i]*m
-    return answer
+    return sum(sorted(score)[len(score)%m::m])*m
