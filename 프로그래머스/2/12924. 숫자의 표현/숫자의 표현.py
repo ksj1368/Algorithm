bc35e7dec3e62181a80b0148a8d32d4d1175b384
Seongjun Jang <83360918+ksj1368@users.noreply.github.com>
2024-06-10 09:14:30 +0900

    [level 2] Title: 숫자의 표현, Time: 4.95 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        n_sum = 0
+        for j in range(i, n+1):
+            n_sum += j
+            if n_sum == n:
+                answer += 1
+            if n_sum > n:
+                break
+    return answer
