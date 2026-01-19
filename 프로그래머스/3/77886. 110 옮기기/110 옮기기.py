d12a3d2bb7ef9add8d5895f5a7973009af4f3a85
Seongjun Jang <83360918+ksj1368@users.noreply.github.com>
2026-01-19 09:54:08 +0900

    [level 3] Title: 110 옮기기, Time: 298.48 ms, Memory: 14.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = []
+    for x in s:
+        stack = []
+        cnt = 0
+        for i in x:
+            stack.append(i)
+            if len(stack) >= 3 and stack[-3:] == ['1', '1', '0']:
+                stack.pop()
+                stack.pop()
+                stack.pop()
+                cnt += 1
+        stack = "".join(stack)
+        zero_idx = stack.rfind('0') # 인덱스 반환, 없을 경우 -1 반환 
+        if zero_idx == -1:
+            answer.append(('110' * cnt) + stack)
+        else:
+            answer.append(stack[:zero_idx+1] + ('110' * cnt ) + stack[zero_idx+1:])
+    return answer
